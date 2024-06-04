19b8ca51b6fe0f9539720bb58a509ad752928937
Gourav Govande <gourav.govande@wisdmlabs.com>
2024-06-04 16:05:06 +0530

    Updating the whatsnew json file url

--- remui/classes/utility.php
+++ remui/classes/utility.php
@@ -1459,10 +1459,9 @@ class utility {
         if(count($availableroles) > 0 || is_role_switched($COURSE->id)) {
             $democontext["hasswitchablerolebtns"] = true;
         } 
-
-        $whatsnew = $CFG->wwwroot. '/theme/remui/whatsnew.json';
+        
+        $whatsnew = "https://demo.tryremui.edwiser.org/whatsnew.json";
         $democontext["whatsnew"] = json_decode(file_get_contents($whatsnew), true);
-
         return $democontext;
     }
 }
